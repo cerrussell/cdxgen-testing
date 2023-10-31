afbd6727387efbdcd85c9b54e69fd29c928419fd
Caroline Russell <caroline@appthreat.dev>
2023-10-31 11:10:33 -0400

    Update python.ps1

--- python.ps1
+++ python.ps1
@@ -3,18 +3,18 @@ git clone https://github.com/Tautulli/Tautulli.git tautulli
 git clone https://github.com/se2p/pynguin.git pynguin
 git clone https://github.com/scrapy/scrapy.git scrapy
 
-mkdir artifacts-$MATRIX_OS
-mkdir artifacts-$MATRIX_OS/spacy
-mkdir artifacts-$MATRIX_OS/tautulli
-mkdir artifacts-$MATRIX_OS/pynguin
-mkdir artifacts-$MATRIX_OS/scrapy
+mkdir artifacts-$env:MATRIX_OS
+mkdir artifacts-$env:MATRIX_OS/spacy
+mkdir artifacts-$env:MATRIX_OS/tautulli
+mkdir artifacts-$env:MATRIX_OS/pynguin
+mkdir artifacts-$env:MATRIX_OS/scrapy
 
-cdxgen -p -t python --deep -o artifacts-$MATRIX_OS/spacy/spacy-bom-$MATRIX_OS.json spacy
-cdxgen -p -t python --deep -o artifacts-$MATRIX_OS/tautulli/tautulli-bom-$MATRIX_OS.json tautulli
-cdxgen -p -t python --deep -o artifacts-$MATRIX_OS/pynguin/pynguin-bom-$MATRIX_OS.json pynguin
-cdxgen -p -t python --deep -o artifacts-$MATRIX_OS/scrapy/scrapy-bom-$MATRIX_OS.json scrapy
+cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/spacy/spacy-bom-$env:MATRIX_OS.json spacy
+cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/tautulli/tautulli-bom-$env:MATRIX_OS.json tautulli
+cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/pynguin/pynguin-bom-$env:MATRIX_OS.json pynguin
+cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/scrapy/scrapy-bom-$env:MATRIX_OS.json scrapy
 
-evinse -p -i artifacts-$MATRIX_OS/spacy/spacy-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/spacy/spacy-evinse-$MATRIX_OS.json -l python --with-reachables spacy
-evinse -p -i artifacts-$MATRIX_OS/tautulli/tautulli-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/tautulli/tautulli-evinse-$MATRIX_OS.json -l python --with-reachables tautulli
-evinse -p -i artifacts-$MATRIX_OS/pynguin/pynguin-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/pynguin/pynguin-evinse-$MATRIX_OS.json -l python --with-reachables pynguin
-evinse -p -i artifacts-$MATRIX_OS/scrapy/scrapy-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/scrapy/scrapy-evinse-$MATRIX_OS.json -l python --with-reachables scrapy
+evinse -p -i artifacts-$env:MATRIX_OS/spacy/spacy-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/spacy/spacy-evinse-$env:MATRIX_OS.json -l python --with-reachables spacy
+evinse -p -i artifacts-$env:MATRIX_OS/tautulli/tautulli-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/tautulli/tautulli-evinse-$env:MATRIX_OS.json -l python --with-reachables tautulli
+evinse -p -i artifacts-$env:MATRIX_OS/pynguin/pynguin-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/pynguin/pynguin-evinse-$env:MATRIX_OS.json -l python --with-reachables pynguin
+evinse -p -i artifacts-$env:MATRIX_OS/scrapy/scrapy-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/scrapy/scrapy-evinse-$env:MATRIX_OS.json -l python --with-reachables scrapy
