376c8ae7a1d02756816973ca2dd88bf6029fcf3a
Caroline Russell <caroline@appthreat.dev>
2023-10-30 23:30:50 -0400

    Update nodejs.ps1

--- nodejs.ps1
+++ nodejs.ps1
@@ -9,12 +9,12 @@ mkdir artifacts-$MATRIX_OS/videojs
 mkdir artifacts-$MATRIX_OS/ava
 mkdir artifacts-$MATRIX_OS/carbon
 
-cdxgen -p -t javascript --deep -o artifacts-$MATRIX_OS/sequelize/sequelize-bom-$MATRIX_OS.json sequelize
-cdxgen -p -t javascript --deep -o artifacts-$MATRIX_OS/videojs/videojs-bom-$MATRIX_OS.json videojs
-cdxgen -p -t javascript --deep -o artifacts-$MATRIX_OS/ava/ava-bom-$MATRIX_OS.json ava
-cdxgen -p -t javascript --deep -o artifacts-$MATRIX_OS/carbon/carbon-bom-$MATRIX_OS.json carbon
+cdxgen -t javascript --deep -o artifacts-$MATRIX_OS/sequelize/sequelize-bom-$MATRIX_OS.json sequelize
+cdxgen -t javascript --deep -o artifacts-$MATRIX_OS/videojs/videojs-bom-$MATRIX_OS.json videojs
+cdxgen -t javascript --deep -o artifacts-$MATRIX_OS/ava/ava-bom-$MATRIX_OS.json ava
+cdxgen -t javascript --deep -o artifacts-$MATRIX_OS/carbon/carbon-bom-$MATRIX_OS.json carbon
 
-evinse -p -i artifacts-$MATRIX_OS/sequelize/sequelize-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/sequelize/sequelize-evinse-$MATRIX_OS.json -l javascript --with-reachables sequelize
-evinse -p -i artifacts-$MATRIX_OS/videojs/videojs-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/videojs/videojs-evinse-$MATRIX_OS.json -l javascript --with-reachables videojs
-evinse -p -i artifacts-$MATRIX_OS/ava/ava-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/ava/ava-evinse-$MATRIX_OS.json -l javascript --with-reachables ava
-evinse -p -i artifacts-$MATRIX_OS/carbon/carbon-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/carbon/carbon-evinse-$MATRIX_OS.json -l javascript --with-reachables carbon
+evinse -i artifacts-$MATRIX_OS/sequelize/sequelize-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/sequelize/sequelize-evinse-$MATRIX_OS.json -l javascript --with-reachables sequelize
+evinse -i artifacts-$MATRIX_OS/videojs/videojs-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/videojs/videojs-evinse-$MATRIX_OS.json -l javascript --with-reachables videojs
+evinse -i artifacts-$MATRIX_OS/ava/ava-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/ava/ava-evinse-$MATRIX_OS.json -l javascript --with-reachables ava
+evinse -i artifacts-$MATRIX_OS/carbon/carbon-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/carbon/carbon-evinse-$MATRIX_OS.json -l javascript --with-reachables carbon
