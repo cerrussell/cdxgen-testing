9e6d6771fa2f95c72796e9b940e3cf063654246b
Caroline Russell <caroline@appthreat.dev>
2023-10-31 10:57:07 -0400

    Update nodejs.ps1

--- nodejs.ps1
+++ nodejs.ps1
@@ -3,18 +3,18 @@ git clone https://github.com/videojs/video.js.git videojs
 git clone https://github.com/avajs/ava ava
 git clone https://github.com/carbon-app/carbon.git carbon
 
-mkdir artifacts-$MATRIX_OS
-mkdir artifacts-$MATRIX_OS/sequelize
-mkdir artifacts-$MATRIX_OS/videojs
-mkdir artifacts-$MATRIX_OS/ava
-mkdir artifacts-$MATRIX_OS/carbon
+mkdir artifacts-$env:MATRIX_OS
+mkdir artifacts-$env:MATRIX_OS/sequelize
+mkdir artifacts-$env:MATRIX_OS/videojs
+mkdir artifacts-$env:MATRIX_OS/ava
+mkdir artifacts-$env:MATRIX_OS/carbon
 
-cdxgen -t javascript --deep -o artifacts-$MATRIX_OS/sequelize/sequelize-bom-$MATRIX_OS.json sequelize
-cdxgen -t javascript --deep -o artifacts-$MATRIX_OS/videojs/videojs-bom-$MATRIX_OS.json videojs
-cdxgen -t javascript --deep -o artifacts-$MATRIX_OS/ava/ava-bom-$MATRIX_OS.json ava
-cdxgen -t javascript --deep -o artifacts-$MATRIX_OS/carbon/carbon-bom-$MATRIX_OS.json carbon
+cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/sequelize/sequelize-bom-$env:MATRIX_OS.json sequelize
+cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/videojs/videojs-bom-$env:MATRIX_OS.json videojs
+cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/ava/ava-bom-$env:MATRIX_OS.json ava
+cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/carbon/carbon-bom-$env:MATRIX_OS.json carbon
 
-evinse -i artifacts-$MATRIX_OS/sequelize/sequelize-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/sequelize/sequelize-evinse-$MATRIX_OS.json -l javascript --with-reachables sequelize
-evinse -i artifacts-$MATRIX_OS/videojs/videojs-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/videojs/videojs-evinse-$MATRIX_OS.json -l javascript --with-reachables videojs
-evinse -i artifacts-$MATRIX_OS/ava/ava-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/ava/ava-evinse-$MATRIX_OS.json -l javascript --with-reachables ava
-evinse -i artifacts-$MATRIX_OS/carbon/carbon-bom-$MATRIX_OS.json -o artifacts-$MATRIX_OS/carbon/carbon-evinse-$MATRIX_OS.json -l javascript --with-reachables carbon
+evinse -i artifacts-$env:MATRIX_OS/sequelize/sequelize-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/sequelize/sequelize-evinse-$env:MATRIX_OS.json -l javascript --with-reachables sequelize
+evinse -i artifacts-$env:MATRIX_OS/videojs/videojs-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/videojs/videojs-evinse-$env:MATRIX_OS.json -l javascript --with-reachables videojs
+evinse -i artifacts-$env:MATRIX_OS/ava/ava-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/ava/ava-evinse-$env:MATRIX_OS.json -l javascript --with-reachables ava
+evinse -i artifacts-$env:MATRIX_OS/carbon/carbon-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/carbon/carbon-evinse-$env:MATRIX_OS.json -l javascript --with-reachables carbon
