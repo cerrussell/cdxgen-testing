4a79230126a1da53b3098b21baa0f484d1f2dbd0
Caroline Russell <caroline@appthreat.dev>
2023-10-30 21:48:10 -0400

    Create python_repos.ps1

---
+++ python_repos.ps1
@@ -0,0 +1,4 @@
+git clone https://github.com/explosion/spaCy.git spacy
+git clone https://github.com/Tautulli/Tautulli.git tautulli
+git clone https://github.com/se2p/pynguin.git pynguin
+git clone https://github.com/scrapy/scrapy.git scrapy
