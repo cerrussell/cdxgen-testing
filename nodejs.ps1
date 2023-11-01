46d527623f465c2df5bc5b4782390aebedac66bd
Caroline Russell <caroline@appthreat.dev>
2023-11-01 13:58:03 -0400

    Make evinse run optional
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

--- nodejs.ps1
+++ nodejs.ps1
@@ -9,12 +9,21 @@ mkdir artifacts-$env:MATRIX_OS/videojs
 mkdir artifacts-$env:MATRIX_OS/ava
 mkdir artifacts-$env:MATRIX_OS/carbon
 
-cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/sequelize/sequelize-bom-$env:MATRIX_OS.json sequelize
-cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/videojs/videojs-bom-$env:MATRIX_OS.json videojs
-cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/ava/ava-bom-$env:MATRIX_OS.json ava
-cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/carbon/carbon-bom-$env:MATRIX_OS.json carbon
+if ($env:WITH_EVINSE) {
+    cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/sequelize/sequelize-bom-$env:MATRIX_OS.json sequelize
+    cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/videojs/videojs-bom-$env:MATRIX_OS.json videojs
+    cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/ava/ava-bom-$env:MATRIX_OS.json ava
+    cdxgen -t javascript --deep -o artifacts-$env:MATRIX_OS/carbon/carbon-bom-$env:MATRIX_OS.json carbon
+    evinse -i artifacts-$env:MATRIX_OS/sequelize/sequelize-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/sequelize/sequelize-evinse-$env:MATRIX_OS.json -l javascript --with-reachables sequelize
+    evinse -i artifacts-$env:MATRIX_OS/videojs/videojs-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/videojs/videojs-evinse-$env:MATRIX_OS.json -l javascript --with-reachables videojs
+    evinse -i artifacts-$env:MATRIX_OS/ava/ava-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/ava/ava-evinse-$env:MATRIX_OS.json -l javascript --with-reachables ava
+    evinse -i artifacts-$env:MATRIX_OS/carbon/carbon-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/carbon/carbon-evinse-$env:MATRIX_OS.json -l javascript --with-reachables carbon
+}
+else {
+    cdxgen -t javascript -o artifacts-$env:MATRIX_OS/sequelize/sequelize-bom-$env:MATRIX_OS.json sequelize
+    cdxgen -t javascript -o artifacts-$env:MATRIX_OS/videojs/videojs-bom-$env:MATRIX_OS.json videojs
+    cdxgen -t javascript -o artifacts-$env:MATRIX_OS/ava/ava-bom-$env:MATRIX_OS.json ava
+    cdxgen -t javascript -o artifacts-$env:MATRIX_OS/carbon/carbon-bom-$env:MATRIX_OS.json carbon
+}
 
-evinse -i artifacts-$env:MATRIX_OS/sequelize/sequelize-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/sequelize/sequelize-evinse-$env:MATRIX_OS.json -l javascript --with-reachables sequelize
-evinse -i artifacts-$env:MATRIX_OS/videojs/videojs-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/videojs/videojs-evinse-$env:MATRIX_OS.json -l javascript --with-reachables videojs
-evinse -i artifacts-$env:MATRIX_OS/ava/ava-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/ava/ava-evinse-$env:MATRIX_OS.json -l javascript --with-reachables ava
-evinse -i artifacts-$env:MATRIX_OS/carbon/carbon-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/carbon/carbon-evinse-$env:MATRIX_OS.json -l javascript --with-reachables carbon
+
--- python.ps1
+++ python.ps1
@@ -9,12 +9,19 @@ mkdir artifacts-$env:MATRIX_OS/tautulli
 mkdir artifacts-$env:MATRIX_OS/pynguin
 mkdir artifacts-$env:MATRIX_OS/scrapy
 
-cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/spacy/spacy-bom-$env:MATRIX_OS.json spacy
-cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/tautulli/tautulli-bom-$env:MATRIX_OS.json tautulli
-cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/pynguin/pynguin-bom-$env:MATRIX_OS.json pynguin
-cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/scrapy/scrapy-bom-$env:MATRIX_OS.json scrapy
-
-evinse -p -i artifacts-$env:MATRIX_OS/spacy/spacy-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/spacy/spacy-evinse-$env:MATRIX_OS.json -l python --with-reachables spacy
-evinse -p -i artifacts-$env:MATRIX_OS/tautulli/tautulli-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/tautulli/tautulli-evinse-$env:MATRIX_OS.json -l python --with-reachables tautulli
-evinse -p -i artifacts-$env:MATRIX_OS/pynguin/pynguin-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/pynguin/pynguin-evinse-$env:MATRIX_OS.json -l python --with-reachables pynguin
-evinse -p -i artifacts-$env:MATRIX_OS/scrapy/scrapy-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/scrapy/scrapy-evinse-$env:MATRIX_OS.json -l python --with-reachables scrapy
+if ($env:WITH_EVINSE) {
+  cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/spacy/spacy-bom-$env:MATRIX_OS.json spacy
+  cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/tautulli/tautulli-bom-$env:MATRIX_OS.json tautulli
+  cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/pynguin/pynguin-bom-$env:MATRIX_OS.json pynguin
+  cdxgen -p -t python --deep -o artifacts-$env:MATRIX_OS/scrapy/scrapy-bom-$env:MATRIX_OS.json scrapy
+  evinse -p -i artifacts-$env:MATRIX_OS/spacy/spacy-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/spacy/spacy-evinse-$env:MATRIX_OS.json -l python --with-reachables spacy
+  evinse -p -i artifacts-$env:MATRIX_OS/tautulli/tautulli-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/tautulli/tautulli-evinse-$env:MATRIX_OS.json -l python --with-reachables tautulli
+  evinse -p -i artifacts-$env:MATRIX_OS/pynguin/pynguin-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/pynguin/pynguin-evinse-$env:MATRIX_OS.json -l python --with-reachables pynguin
+  evinse -p -i artifacts-$env:MATRIX_OS/scrapy/scrapy-bom-$env:MATRIX_OS.json -o artifacts-$env:MATRIX_OS/scrapy/scrapy-evinse-$env:MATRIX_OS.json -l python --with-reachables scrapy
+}
+else {
+  cdxgen -p -t python -o artifacts-$env:MATRIX_OS/spacy/spacy-bom-$env:MATRIX_OS.json spacy
+  cdxgen -p -t python -o artifacts-$env:MATRIX_OS/tautulli/tautulli-bom-$env:MATRIX_OS.json tautulli
+  cdxgen -p -t python -o artifacts-$env:MATRIX_OS/pynguin/pynguin-bom-$env:MATRIX_OS.json pynguin
+  cdxgen -p -t python -o artifacts-$env:MATRIX_OS/scrapy/scrapy-bom-$env:MATRIX_OS.json scrapy
+}
