f45f80c43e63be8e5c886d5d72cc92eab7cab438
Caroline Russell <caroline@appthreat.dev>
2024-05-13 11:32:34 -0400

    Add a python test.
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

---
+++ test/diff/diff_tests.py
@@ -0,0 +1,15 @@
+import json
+
+from custom_json_diff.custom_json_diff import sort_dict
+
+
+# @pytest.fixture
+def sample_bom(json_file):
+    with open(json_file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return sort_dict(data)
+
+
+def test_java_bom():
+    assert sample_bom('java-sec-code-bom.json') == sample_bom('/home/runner/work/cdxgen-samples/java-sec-code-bom.json')
+
