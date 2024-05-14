61d463291e4306c6f436ae192152f7ae019611f8
Caroline Russell <caroline@appthreat.dev>
2024-05-14 00:17:18 -0400

    Fix pytest.
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

--- test/diff/diff_tests.py
+++ test/diff/diff_tests.py
@@ -1,15 +1,16 @@
-import json
-
-from custom_json_diff.custom_json_diff import sort_dict
-
-
-# @pytest.fixture
-def sample_bom(json_file):
-    with open(json_file, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    return sort_dict(data)
+from custom_json_diff.custom_json_diff import compare_dicts
 
 
 def test_java_bom():
-    assert sample_bom('test/diff/java-sec-code-bom.json') == sample_bom('/home/runner/work/cdxgen-samples/java-sec-code-bom.json')
+    assert compare_dicts('test/diff/java-sec-code-bom.json', '/home/runner/work/cdxgen-samples/java-sec-code-bom.json', False,
+                         {"serialNumber", "metadata.timestamp"}, False) == "JSON files are equal"
 
+
+def test_python_bom():
+    assert compare_dicts('test/diff/django-goat-bom.json', '/home/runner/work/cdxgen-samples/django-goat-bom.json', False,
+                         {"serialNumber", "metadata.timestamp"}, False) == "JSON files are equal"
+
+
+def test_javascript_bom():
+    assert compare_dicts('test/diff/node-goat-bom.json', '/home/runner/work/cdxgen-samples/node-goat-bom.json', False,
+                         {"serialNumber", "metadata.timestamp"}, False) == "JSON files are equal"
